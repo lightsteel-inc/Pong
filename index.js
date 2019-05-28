02423413d33cfccfd789b5e9376ae5cc688adf66
Maxsteelcooldan <46035576+Maxsteelcooldan@users.noreply.github.com>
2019-05-28 17:25:39 -0400

    Update index.js

--- index.js
+++ index.js
@@ -1,17 +1,40 @@
 var canvas;
 var ctx;
-var Ballx;
-var PaddleX;
+var BallX;
+var Paddle1X;
+var Paddle2Y;
+
   window.onload = function() {
     canvas = document.getElementById('canvas');
     ctx = canvas.getContext('2d');
-    setInterval(draw, 1000/30);
+    setInterval(draw, 1000/60);
+    document.addEventListener("keydown",controller);
   };
   function draw() {
+    Paddle1Y = 200;
+    Paddle2Y = 200;
+    BallX = 250;
     ctx.fillStyle = 'black';
     ctx.fillRect(0,0,canvas.width,canvas.height);
     ctx.fillStyle = 'blue';
-    ctx.beginPath();
-    ctx.arc(10,100,10,5,Math.Pi*2, true);
-    ctx.fill();
-};
+    ctx.fillRect(10,Paddle1Y,10,100);
+    ctx.fillRect(480,Paddle2Y,10,100);
+    ctx.beginPath()
+    ctx.arc(BallX, 250, 10, 0, 2 * Math.PI, true);
+    ctx.fill()
+    ctx.closePath();
+
+}
+  function controller(evt) {
+    switch(evt.keyCode) {
+    case 83:
+        Paddle1Y = Paddle1Y + 10;
+        break;
+    case 38:
+        break;
+      case 39:
+        break;
+    case 40:
+        break;
+  }
+ }
