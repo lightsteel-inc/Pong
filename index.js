a1763a008d1d3b5320c27418823148db416ce7cd
Maxsteelcooldan <46035576+Maxsteelcooldan@users.noreply.github.com>
2019-05-27 21:55:20 -0400

    Update index.js

--- index.js
+++ index.js
@@ -1,17 +1,17 @@
-let Ballx = 50;
-window.onload = function() {
-  let canvas = document.getElementById('canvas')
-  let ctx = canvas.getContext('2d')
-  setInterval(draw, 1000/60)
-  }
-function AI() {
-  
-}
-function draw() {
-  ctx.fillRect(0,0,canvas.width,canvas.height)
-  ctx.fillStyle = 'white'
-  ctx.fillRect(10,10,10,10)
-  Ballx += 5
-  ctx.arc(Ballx,100,40,0,Math.PI*2, true)
-}
-function 
+var canvas;
+var ctx;
+var Ballx;
+var PaddleX;
+  window.onload = function() {
+    canvas = document.getElementById('canvas');
+    ctx = canvas.getContext('2d');
+    setInterval(draw, 1000/30);
+  };
+  function draw() {
+    ctx.fillStyle = 'black';
+    ctx.fillRect(0,0,canvas.width,canvas.height);
+    ctx.fillStyle = 'blue';
+    ctx.beginPath();
+    ctx.arc(10,100,10,5,Math.Pi*2, true);
+    ctx.fill();
+};
