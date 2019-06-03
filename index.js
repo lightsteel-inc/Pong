97a24256328117c7893a118956e01ef9feaaa247
LightspeedCoder <48801499+LightspeedCoder@users.noreply.github.com>
2019-06-03 10:23:46 -0400

    Update index.js

--- index.js
+++ index.js
@@ -57,7 +57,15 @@ var Ball;
       }
     }
 //}
-  function collision() {
+ /* function collision(a, b) {
+    return !(
+        ((a.y + a.height) < (b.y)) ||
+        (a.y > (b.y + b.height)) ||
+        ((a.x + a.width) < b.x) ||
+        (a.x > (b.x + b.width))
+   );
+       */ //collision setup
+}
 
 }
     
