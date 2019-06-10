c963dfa310472d1353df0d5a7b8061a693ed64f9
Maxsteelcooldan <46035576+Maxsteelcooldan@users.noreply.github.com>
2019-06-10 15:57:00 -0400

    Update index.js

--- index.js
+++ index.js
@@ -64,9 +64,9 @@ var Balltwo
         BallX = 250
    }
     if(BallY > canvas.height) {
-       BallY = false
-    } else if (BallY < 0) {
        BallY = true
+    } else if (BallY < 0) {
+       BallY = false
     }
   }//end of function move
 
